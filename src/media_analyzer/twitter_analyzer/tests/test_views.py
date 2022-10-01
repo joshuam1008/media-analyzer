49df095e54a2c82642e238a9e5cc8dda9567d97c
lamonkey <88therisingsun@gmail.com>
2022-10-01 19:13:51 -0400

    config #3 config static_root

---
+++ src/media_analyzer/twitter_analyzer/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.test import Client
+# Create your tests here.
+# Adapted from https://test-driven-django-development.readthedocs.io/en/latest/03-views.html#the-homepage-test
+'''
+mimic user behavior testing views and api 
+'''
+class TestViews(TestCase):
+    def test_homepage(self):
+        client = Client()
+        response = client.get('/twitter/')
+        self.assertEqual(response.status_code, 200)
