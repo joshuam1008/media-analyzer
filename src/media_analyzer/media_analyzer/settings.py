04d17827c10b6cb3e23677850dc7f0ba9910f1f3
lamonkey <88therisingsun@gmail.com>
2022-10-01 20:17:55 -0400

    config for deployment

--- src/media_analyzer/media_analyzer/settings.py
+++ src/media_analyzer/media_analyzer/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rqt_spmx@%q1gwd6r9+0di=mah9fpxte&deqb&50@8b8&(%$2k'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
