de2dad3c9d32834aedc75da22ef7650e8e59d7c9
cjdolan <cjdolan@ncsu.edu>
2022-10-05 00:59:34 -0400

    Made prediction function static and initialized model during import

--- src/inference.py
+++ src/inference.py
@@ -10,49 +10,33 @@ import pandas as pd
 import time
 from tqdm import tqdm
 
-class SentimentPredictor():
-    def __init__(self, model_dir, model_name='prajjwal1/bert-mini'):
-        self.model_name = model_name
-        self.model = TwitterSentimentModel(model_name=self.model_name)
-        self.model.cpu()
-        self.model.load_state_dict(torch.load(model_dir))
+model_name = 'prajjwal1/bert-mini'
+model_dir = '../train/checkpoints/TwitterSentimentModel.pt'
+Sentiment = TwitterSentimentModel(model_name=model_name)
+Sentiment.cpu()
+Sentiment.load_state_dict(torch.load(model_dir))
 
-    def read_data(self, data):
-        '''
-        Load the data into the cuda device
-        '''
-        return tuple(x.cpu() for x in data)
+def make_prediction(data_sample):
+    '''
+    Run the model on a data sample
+        - data_sample: A list of tweets to build inference on
+    returns: A list of enumerated values (NEGATIVE, NEUTRAL, POSITIVE)
+    '''
 
-    def get_pred(self, data):
-        '''
-        Set the prediction to 1 if the prediction exceeds 0.5 probability
-        '''
-        results = []
-        for d in data:
-            results.append(np.argmax(d))
-        return results
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    pair = {0:'NEGATIVE', 1:'NEUTRAL', 2:'POSITIVE'}
+    predicts = []
+    for data in data_sample:
+        input = tokenizer.encode_plus(text = data,
+                                        add_special_tokens=True,
+                                        padding='max_length',
+                                        max_length = 64,
+                                        return_tensors='pt',
+                                        truncation=True,
+                                        return_attention_mask=True)
 
-    def make_prediction(self, data_sample):
-        '''
-        Run the model on a data sample
-         - data_sample: A list of tweets to build inference on
-        returns: A list of enumerated values (NEGATIVE, NEUTRAL, POSITIVE)
-        '''
 
-        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        pair = {0:'NEGATIVE', 1:'NEUTRAL', 2:'POSITIVE'}
-        predicts = []
-        for data in tqdm(data_sample):
-            input = tokenizer.encode_plus(text = data,
-                                            add_special_tokens=True,
-                                            padding='max_length',
-                                            max_length = 64,
-                                            return_tensors='pt',
-                                            truncation=True,
-                                            return_attention_mask=True)
-
-  
-            preds = self.model(input['input_ids'].cpu(), input['attention_mask'].cpu()).detach().numpy().ravel()
-            p = np.argmax(preds)
-            predicts.append(pair[p])
-        return predicts
+        preds = Sentiment(input['input_ids'].cpu(), input['attention_mask'].cpu()).detach().numpy().ravel()
+        p = np.argmax(preds)
+        predicts.append(pair[p])
+    return predicts
