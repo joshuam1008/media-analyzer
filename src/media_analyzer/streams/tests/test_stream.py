c106e268b02e37a4f4e581707f35919db337cb90
david12white <david12white@gmail.com>
2022-10-05 19:36:02 -0400

    Increased sleep time on test to allow more tweet population

---
+++ src/media_analyzer/streams/tests/test_stream.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+import time, sys
+
+# Add module to path
+sys.path.append('../twitter_analyzer')
+from streams.twitter_stream import TwitterStream
+
+# IMPORTANT: Class uses bearer token to initialize new stream. This counts towards the limits
+# imposed by the twitter stream API. Don't run this test too much.
+class TestStream(TestCase):
+    '''
+    Tests the Twitter Stream.
+    '''
+    def test_stream_responses(self):
+
+        # Initialize the Stream
+        stream = TwitterStream()
+
+        # Start the Stream
+        stream.toggle_module()
+
+        # Wait for Results
+        time.sleep(4)
+
+        # Disconnect the Stream
+        stream.disconnect()
+
+        # Get Results of Stream.
+        results = stream.result_generator()
+
+        # Ensure we got something.
+        self.assertTrue(len(results) > 0)
+
+        # Ensure we got strings.
+        first_result = results[0]
+        self.assertTrue(isinstance(first_result, str))
