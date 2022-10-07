9b0f13a776f73871f388bc63329623db3bf343b8
lamonkey <88therisingsun@gmail.com>
2022-10-07 17:56:26 -0400

    #6 enhanced frontend

--- src/media_analyzer/twitter_analyzer/static/twitter_analyzer/main.js
+++ src/media_analyzer/twitter_analyzer/static/twitter_analyzer/main.js
@@ -1,64 +1,76 @@
-var categories = ['stream', 'sentiment']
-var ids = []
-let summary = {
-    positive: 0,
-    negative: 0,
-    neutral: 0
+var summary = {}
+var selected_words = []
+//modified upon button click
+var categories = {
+    'stream': false,
+    'sentiment': false,
+    'language': false
 }
-// listen to button click and send value to api
+//missing data by category
+var missing_categories = {
+    'stream': 0,
+    'sentiment': 0,
+    'language': 0
+}
+//stream rate
+var stream_rate = 0
+//which overal reuslt to generate
+var summary_category = null
+//twitter with none filed
+var ids = []
+var total_tweet = 0
+//all tweet recieved
+all_tweets = {}
+id = 0
 
-stream_active = false;
-fetch_every_second = null;
-
-// google.charts.load('current', {packages: ['corechart']});
-// google.charts.setOnLoadCallback(drawChart);
-
-
-$(".stream-ctrl").click(async function () {
-    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-
-    //raw button value from its html 'value' property
-    let button_value = $(this).val();
-    //split the buttton value by its spacing (such as "stream 1" to [stream, 1])
-    button_value_as_arr = button_value.split(" ");
-
-    //the first element of the value property- its name
-    button_value_name = button_value_as_arr[0];
-
-    //the second element of the value property- the value
-    button_value_integer = parseInt(button_value_as_arr[1]);
-
-    // if stream being toggled, toggle stream_active
-    if (button_value_integer == 0) {
-        stream_active = !stream_active;
-        // change button text to reflect stream status
-        if (stream_active) {
-            fetch_every_second = setInterval(fetch_result, 1000);
-            $(this).text("Stop Analysis");
-        } else {
-            clearInterval(fetch_every_second);
-            $(this).text("Start Analysis");
+//toggle result
+$(".nav-link").click(function () {
+    let text = $(this).text()
+    //change text for buttom
+    text = text.split(" ");
+    if (text[0] == "Start") {
+        text[0] = "Stop"
+    } else if (text[0] == "Stop") {
+        text[0] = "Start"
+    }
+    //toggle module
+    let key = text[1].toLowerCase()
+    if (key in categories) {
+        categories[key] = !categories[key]
+        $(this).html(text.join(" "))
+    }
+})
+//save the tweet locally and static
+var save_tweets = function (json_response) {
+    let stream = json_response.stream
+    stream_rate = Object.keys(stream).length / 2
+    let inds = json_response.inds
+    for (data of [stream, inds]) {
+        for (id of Object.keys(data)) {
+            if (!(id in all_tweets)) {
+                all_tweets[id] = {}
+            }
+            for (category of Object.keys(data[id]))
+                all_tweets[id][category] = data[id][category]
+            //if result is none put twitter id into ids
+            if (data[id][category] == null) {
+                //do nothing for now
+            }
         }
     }
 
-    //post a request to the server with the button details
-    const response = await fetch('/twitter/toggle_modules', {
-        method: 'POST',
-        body: JSON.stringify({
-            name: button_value_name,
-            state: button_value_integer
-        }),
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-            "X-CSRFToken": csrftoken
-        }
-    })
-    // alert(`module is ${value[0]} value is ${value[1]}.`);
-});
-
-var add_tweet_to_ui = (tweet) => {
+}
+//generate static of the stream
+var get_static = function () {
+    //update ui
+    $("#rate").html(`Rate: ${stream_rate}/min`)
+    $("#sentiment_count").html(`sentiment: ${missing_categories['sentiment']}`)
+    $("#twitter_count").html(`Total: ${total_tweet}`)
+}
+//add tweet to ui
+var add_tweet_to_ui = (tweet_id, tweet, position) => {
     const tweet_as_link = document.createElement("a");
+    tweet_as_link.id = tweet_id
     tweet_as_link.classList.add("list-group-item");
     tweet_as_link.classList.add("list-group-item-action");
 
@@ -73,28 +85,34 @@ var add_tweet_to_ui = (tweet) => {
         tweet_as_link.classList.add("list-group-item-dark");
     }
 
-    const list = document.querySelector('#tweets');
-    list.appendChild(tweet_as_link);
+
+    position.appendChild(tweet_as_link);
+    const list_2 = document.querySelector('#tweet_filtered');
+    list_2.appendChild(tweet_as_link.cloneNode(true));
 }
 
+
 //note: chen's code I (David) adapted.
-var generate_overall_sentiment_result = function(position, overall_sentiment_results){
+var plot_histo = function (position, overall_sentiment_results, title) {
 
     //convert dictionary to format of datatable
-    var convert_dict_to_data_table = function(overal_sentiment_results){
+    var convert_dict_to_data_table = function (overal_sentiment_results) {
         let single_result = overall_sentiment_results
-        let data = [['Emo Level','Counts']]
-        for(let [category,value] of Object.entries(single_result)){
-            data.push([category,value])
+        let data = [
+            ['Emo Level', 'Counts']
+        ]
+        for (let [category, value] of Object.entries(single_result)) {
+            data.push([category, value])
         }
         return data
     }
-    function plot_barchart(position,overall_sentiment_results) {
-        function drawChart(tag_name,data) {
+
+    function plot_barchart(position, overall_sentiment_results) {
+        function drawChart(tag_name, data) {
             let histogram_data = google.visualization.arrayToDataTable(data);
-    
+
             let options = {
-                title: 'Overall Sentiment',
+                title: title,
                 legend: {
                     position: 'none'
                 },
@@ -107,22 +125,27 @@ var generate_overall_sentiment_result = function(position, overall_sentiment_res
             packages: ["corechart"]
         });
         google.charts.setOnLoadCallback(
-            () =>{drawChart(position, convert_dict_to_data_table(overall_sentiment_results))
+            () => {
+                drawChart(position, convert_dict_to_data_table(overall_sentiment_results))
             });
-    
 
-    
+
+
     }
-    plot_barchart(position,overall_sentiment_results)
+    plot_barchart(position, overall_sentiment_results)
 }
 
 var fetch_result = async function () {
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    const keys = Object.keys(categories);
+    const request_categories = keys.filter(function (key) {
+        return categories[key]
+    });
     const response = await fetch('/twitter/fetch_result', {
         method: 'POST',
         body: JSON.stringify({
             id: ids,
-            category: categories
+            category: request_categories
         }),
         headers: {
             'Accept': 'application/json, text/plain, */*',
@@ -133,20 +156,104 @@ var fetch_result = async function () {
 
     const add_tweet = (tweet_id) => {
         tweet = json_response.stream[tweet_id];
-        console.log(tweet);
-        if (tweet && tweet.text && tweet.sentiment) {
-            add_tweet_to_ui(tweet);
-            summary[tweet.sentiment.toLowerCase()]++;
+        let position = document.querySelector('#tweet_stream');
+        if (tweet && tweet.text) {
+            add_tweet_to_ui(tweet_id, tweet, position);
+            // summary[tweet.sentiment.toLowerCase()]++;
         }
     };
 
     let json_response = await response.json();
-    // console.log(json_response.stream);
+    //save result
+    save_tweets(json_response)
     let new_tweet_ids = Object.keys(json_response.stream);
-
+    total_tweet += new_tweet_ids.length
     new_tweet_ids.forEach(add_tweet);
-
-    generate_overall_sentiment_result(document.querySelector("#insert-chart"), summary)
-
-    // console.log(json_response);
 }
+
+var add_key_word = function () {
+    let keyword = $("#input_keyword").val()
+    $("#input_keyword").val("")
+    if (keyword != "") {
+        selected_words.push(keyword)
+        $("#keywords").append(`<button type="button" class="btn btn-info btn-sm keyword" onclick="delete_key_word(this)">${keyword}</button>`)
+    }
+    console.log(selected_words)
+}
+
+// add keyword
+$("#submit").click(function () {
+    add_key_word()
+})
+//remove keyword
+var delete_key_word = function (keyword) {
+    const parent = keyword.parentNode
+    parent.removeChild(keyword)
+    const index = selected_words.indexOf(keyword.textContent);
+    if (index !== -1) {
+        selected_words.splice(index, 1);
+    }
+    console.log(selected_words)
+}
+//select which summary to generate
+$(".summary").click(function () {
+    summary_category = $(this).val()
+})
+//generate plot based on summary_category
+var generate_plot = function () {
+    if (summary_category){
+        if (Object.keys(summary).length != 0) {
+            const position = document.querySelector("#insert_chart")
+            let title = null
+            if (summary_category == 'sentiment') {
+                title = "Sentiment Count"
+                //plot sentiment
+                plot_histo(position, summary, title)
+            } else if (summary_category == 'language') {
+                title = "Language Count"
+                //plot lang
+                plot_histo(position, summary, title)
+            } 
+            summary = {}
+        }
+        else{
+            $("#insert_chart").html('<h5 class="text-center" >Waiting on data</h5>')
+        }
+    }
+    else{
+        $("#insert_chart").html('<h5 class="text-center" >Select a category first</h5>')
+    }
+
+
+
+    
+}
+//generate summary based on summary_category
+//add id to ids if the category is missing
+var generate_summary = function () {
+    if (summary_category) {
+        for (id of Object.keys(all_tweets)) {
+            const key = all_tweets[id][summary_category]
+            if (key) {
+                let current_value = summary[key] ?? 0
+                summary[key] = current_value + 1
+            } else {
+                missing_categories[summary_category] += 1
+                //TODO add to ids, but have to remove it when recieve 
+                // ids.push(parseInt(id))
+            }
+        }
+    }
+}
+setInterval(
+    function () {
+        //fetch result
+        fetch_result();
+        //get static 
+        get_static();
+        //generate summary of result
+        generate_summary()
+        //generate plot
+        generate_plot()
+    }, 2000
+);
--- src/media_analyzer/twitter_analyzer/tests/test_sentiment.py
+++ src/media_analyzer/twitter_analyzer/tests/test_sentiment.py
@@ -1,55 +1,55 @@
-# from django.test import TestCase
-# from ..views import stream_cache, data_base
-# from ..tasks import get_sentiment
+from django.test import TestCase
+from ..views import stream_cache, data_base
+from ..tasks import get_sentiment
 
-# # Add module to path
-# # sys.path.append("../twitter_analyzer")
+# Add module to path
+# sys.path.append("../twitter_analyzer")
 
-# # Add parent module to path
-# # sys.path.append("../twitter_analyzer/tasks")
-
-
-# # Create your tests here.
-# # Adapted from https://test-driven-django-development.readthedocs.io/en/latest/03-views.html#the-homepage-test
+# Add parent module to path
+# sys.path.append("../twitter_analyzer/tasks")
 
 
-# class TestSentiment(TestCase):
-#     """
-#     test sentiment storing and access
-#     """
+# Create your tests here.
+# Adapted from https://test-driven-django-development.readthedocs.io/en/latest/03-views.html#the-homepage-test
 
-#     def test_valid_value_generation_cache(self):
-#         id = "1"
-#         text = "TEST TEXT ONE"
-#         # Put an item into the stream cache
-#         stream_cache.put({"id": id, "text": text})
 
-#         get_sentiment(stream_cache=stream_cache, db=data_base)
+class TestSentiment(TestCase):
+    """
+    test sentiment storing and access
+    """
 
-#         result_object = stream_cache.get()
-#         while (not stream_cache.empty()) and result_object["id"] != id:
-#             self.assertIsNone(result_object.get("sentiment"))
-#             result_object = stream_cache.get()
-#         sentiment = result_object.get("sentiment")
-#         self.assertTrue(
-#             (sentiment == "NEUTRAL")
-#             or (sentiment == "POSITIVE")
-#             or (sentiment == "NEGATIVE")
-#         )
+    def test_valid_value_generation_cache(self):
+        id = "1"
+        text = "TEST TEXT ONE"
+        # Put an item into the stream cache
+        stream_cache.put({"id": id, "text": text})
 
-#     def test_valid_value_generation_db(self):
-#         id = "2"
-#         text = "TEST TEXT TWO"
+        get_sentiment(stream_cache=stream_cache, db=data_base)
 
-#         # Put an item into the database
-#         data_base[id] = {"id": id, "text": text}
+        result_object = stream_cache.get()
+        while (not stream_cache.empty()) and result_object["id"] != id:
+            self.assertIsNone(result_object.get("sentiment"))
+            result_object = stream_cache.get()
+        sentiment = result_object.get("sentiment")
+        self.assertTrue(
+            (sentiment == "NEUTRAL")
+            or (sentiment == "POSITIVE")
+            or (sentiment == "NEGATIVE")
+        )
 
-#         get_sentiment(stream_cache=stream_cache, id="2", db=data_base)
+    def test_valid_value_generation_db(self):
+        id = "2"
+        text = "TEST TEXT TWO"
 
-#         result_object = data_base[id]
-#         sentiment = result_object.get("sentiment")
-#         self.assertTrue(
-#             (sentiment == "NEUTRAL")
-#             or (sentiment == "POSITIVE")
-#             or (sentiment == "NEGATIVE")
-#         )
+        # Put an item into the database
+        data_base[id] = {"id": id, "text": text}
+
+        get_sentiment(stream_cache=stream_cache, id="2", db=data_base)
+
+        result_object = data_base[id]
+        sentiment = result_object.get("sentiment")
+        self.assertTrue(
+            (sentiment == "NEUTRAL")
+            or (sentiment == "POSITIVE")
+            or (sentiment == "NEGATIVE")
+        )
