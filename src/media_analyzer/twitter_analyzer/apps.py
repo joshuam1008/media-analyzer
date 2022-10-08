32d1490370381a4a1e38e5eb3baee59b0650c7d6
lamonkey <88therisingsun@gmail.com>
2022-10-08 08:45:17 -0400

    added more test case, use env var as flag to have diff setting

--- src/media_analyzer/twitter_analyzer/apps.py
+++ src/media_analyzer/twitter_analyzer/apps.py
@@ -8,19 +8,32 @@ class TwitterAnalyzerConfig(AppConfig):
     name = 'twitter_analyzer'
 
     def ready(self):
-        # allow for the second time
-        run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE')
-        if run_once is None:
-            os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True'
-            return
+        # init for test
+        run_test = os.environ.get("RUN_TEST")
+        if run_test:
+            print("setting up app")
+            from twitter_analyzer.scheduler import background_scheduler
+            print("initialize scheduler")
+            background_scheduler.start_scheduler()
 
-        print("setting up app")
-        from twitter_analyzer.scheduler import background_scheduler
-        print("initialize scheduler")
-        background_scheduler.start_scheduler()
+            # Start stream
+            from streams.twitter_stream import stream
 
-        # Start stream
-        from streams.twitter_stream import stream
+            print("initialize stream")
+            stream.toggle_module()
+        else: # only init after first reload
+            run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE')
+            if run_once is None:
+                os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True'
+                return
 
-        print("initialize stream")
-        stream.toggle_module()
+            print("setting up app")
+            from twitter_analyzer.scheduler import background_scheduler
+            print("initialize scheduler")
+            background_scheduler.start_scheduler()
+
+            # Start stream
+            from streams.twitter_stream import stream
+
+            print("initialize stream")
+            stream.toggle_module()
--- src/media_analyzer/twitter_analyzer/tests/test_views.py
+++ src/media_analyzer/twitter_analyzer/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test import Client
 import json
 import time
-from twitter_analyzer.scheduler import background_scheduler 
+from twitter_analyzer.scheduler import background_scheduler
 from streams.twitter_stream import stream
 # Create your tests here.
 # Adapted from https://test-driven-django-development.readthedocs.io/en/latest/03-views.html#the-homepage-test
@@ -13,55 +13,80 @@ mimic user behavior testing views and api
 
 class TestViews(TestCase):
     def setUp(self):
-    
-        self.client = Client()  # Django's TestCase already sets self.client so this line isn't required
-
-
+        # background_scheduler.start_scheduler()
+        # Django's TestCase already sets self.client so this line isn't required
+        self.client = Client()
+        # stream.toggle_module()
     """Tests the routing/URLs of the site."""
+    def tests(self):
+        #cap the first letter to avoid been auto executed
+        print("test home page")
+        self.Test_homepage()
+        print("test stream")
+        self.Test_stream()
+        print("test sentiment")
+        self.Test_sentiment()
+        print("test lang")
+        self.Test_lang()
+        #put this at end to stop stand alone threae
+        print("closing thread")
+        self.Test_wrap_up()
 
-    def test_homepage(self):
+    def Test_homepage(self):
         """Tests the homepage URL."""
         response = self.client.get("/twitter/")
         self.assertEqual(response.status_code, 200)
+    
     """Test stream api"""
-    # def test_stream(self):
-    #     time.sleep(1)  # wait for the server
-    #     response = self.client.post('/twitter/fetch_result', {"id": [], "category": ['stream']})
-    #     stream = json.loads(response.content)
-    #     print("recieved")
-    #     print(stream)
-    #     self.assertTrue(len(stream['stream'].keys()) != 0)
+    def Test_stream(self):
+        #try three time 
+        for _ in range(3):
+            time.sleep(1)
+            response = self.client.post('/twitter/fetch_result', json.dumps(
+                {"id": [], "category": ['stream']}), content_type='application/json')
+            data = response.json()
+            self.assertEqual(response.status_code, 200)
+            if len(data['stream'].keys()) > 0:
+                return True
+        return False
+    '''Test if backend can generate sentiment'''
+    def Test_sentiment(self):
+         for _ in range(3):
+            time.sleep(1)
+            response = self.client.post('/twitter/fetch_result', json.dumps(
+                {"id": [], "category": ['stream','sentiment']}), content_type='application/json')
+            data = response.json()
+            self.assertEqual(response.status_code, 200)
+            #check if have sentiment
+            for id in data['stream'].keys():
+                if 'sentiment' in data['stream'][id]:
+                    sentiment = data['stream'][id]['sentiment']
+                    if sentiment is not None:
+                        if 'NEGATIVE' in sentiment or 'NEUTRAL' in sentiment or 'POSITIVE' in sentiment:
+                            return True
 
-
-
-
+         return False
+    '''Test if backend can generate language result'''
+    def Test_lang(self):
+         for _ in range(3):
+            time.sleep(1)
+            response = self.client.post('/twitter/fetch_result', json.dumps(
+                {"id": [], "category": ['stream','lang']}), content_type='application/json')
+            data = response.json()
+            self.assertEqual(response.status_code, 200)
+            #check if have sentiment
+            for id in data['stream'].keys():
+                if 'lang' in data['stream'][id]:
+                    lang_tag = ""
+                    lang_tag = data['stream'][id].get('lang',None)
+                    if lang_tag is not None:
+                        return True
     """Put at buttom to end other thread"""
-    def test_wrap_up(self):
+    def Test_wrap_up(self):
         background_scheduler.stop_scheduler()
         stream.disconnect()
-        stream.worker.join()
+        if stream.worker is not None:
+            stream.worker.join()
         self.assertTrue(background_scheduler.background_scheduler is None)
+    
 
-    # def test_stream(self):
-    #     time.sleep(1)  # wait for the server
-    #     response = self.client.post('/twitter/fetch_result', {"id": [], "category": ['stream']})
-    #     stream = json.loads(response.content)
-    #     print("recieved")
-    #     print(stream)
-    #     self.assertTrue(len(stream['stream'].keys()) != 0)
-
-    # def test_sentiment(self):
-    #     python_dict = {"id": [], "category": ['stream']}
-    #     response = self.client.post('/twitter/fetch_result/',
-    #                                 json.dumps(python_dict),
-    #                                 content_type="application/json")
-    #     data = json.loads(response)
-    #     stream = data['stream']
-    #     id = stream.keys()[0]
-    #     python_dict = {"id": [id], "category": ['sentiment']}
-    #     response = self.client.post('/twitter/fetch_result/',
-    #                                 json.dumps(python_dict),
-    #                                 content_type="application/json")
-    #     sentiment = json.loads(response)['inds'][id]['sentiment']
-    #     self.assertTrue(sentiment != "NEGATIVE" or sentiment !=
-    #                     "NEUTRAL" or sentiment != "POSITIVE")
